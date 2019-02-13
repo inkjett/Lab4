54867bb3d348f1f3bc002a5540a4f7368393f86b
inkjett <mail22sms@gmail.com>
2019-02-13 13:57:37 +0500

    выполнил сложение элементов выше побочной диагонали

--- Lab4/Form1.Designer.cs
+++ Lab4/Form1.Designer.cs
@@ -40,6 +40,9 @@
             this.label5 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -145,14 +148,44 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 142);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(458, 237);
+            this.dataGridView1.Size = new System.Drawing.Size(466, 237);
             this.dataGridView1.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(631, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Посчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(553, 29);
+            this.label6.MaximumSize = new System.Drawing.Size(250, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(235, 26);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Посчитать сумму выше побочной диагонали (матриаца должна быть квадратной)";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(618, 58);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 21;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox5);
@@ -188,6 +221,9 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- Lab4/Form1.cs
+++ Lab4/Form1.cs
@@ -17,10 +17,10 @@ namespace Lab4
         //List<int> Arr_Main = new List<int>();
         //Arr_Main = new List<int>();
         Int32[,] Arr_Main;
-
+        Int32 Size1, Size2;
         //ArrayList Arr_Main;
 
-        
+
         public void Arr_random_gen(Int32 a, Int32 b, ref Int32 [,] arr_out) // метод заполнения массивва рандомными числами
         {
             Random rnd = new Random();
@@ -28,7 +28,7 @@ namespace Lab4
                 for (int j = 0; j < arr_out.GetLength(1); j++)
                     arr_out[i,j] = rnd.Next(a, b);
         }
-        public void Arr_to_Form(int[,] arr_in, ref DataGridView out_Data_Grid)
+        public void Arr_to_Form(int[,] arr_in, ref DataGridView out_Data_Grid)//метод вывода масива в таблицу
         {
             out_Data_Grid.RowCount = arr_in.GetLength(0);
             out_Data_Grid.ColumnCount = arr_in.GetLength(1);
@@ -42,6 +42,23 @@ namespace Lab4
         }
 
 
+        public void sum_diag(int [,] arr_in, ref TextBox summ_out) // посчитать сумму элементво выше диагонали, матриаца должна быть квадратной
+        {
+            Int32 summ = 0;
+            Int32 k = 1;
+
+            for (int i = 0; i < Size1 - 1; i++)
+            {
+                for (int j = 0; j < Size2 - k; j++)
+                {
+                summ += arr_in[i, j];
+                }
+                k++;
+            }
+            summ_out.Text = Convert.ToString(summ);
+            
+        }
+
 
         public Form1()
         {
@@ -50,6 +67,8 @@ namespace Lab4
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.RowHeadersVisible = false;
         }
 
                           
@@ -59,16 +78,23 @@ namespace Lab4
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {            
+        private void button2_Click(object sender, EventArgs e) //генерация таблицы
+        {
+            Size1 = Int32.Parse(textBox1.Text);
+            Size2 = Int32.Parse(textBox5.Text);
             Arr_Main = new Int32[Int32.Parse(textBox1.Text), Int32.Parse(textBox5.Text)];
             Arr_random_gen(Int32.Parse(textBox2.Text), Int32.Parse(textBox3.Text),ref Arr_Main );
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e) // вывод данных в таблицу
         {
             Arr_to_Form(Arr_Main,ref dataGridView1);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e) // расчет саммы элементов матрицы
+        {
+            sum_diag(Arr_Main, ref textBox4);
         }
     }
 }
